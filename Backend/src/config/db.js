5ad88d900922d503874274ecdc9668ff1d963083
Mangal Kumar <mangal192004@gmail.com>
2026-05-28 14:21:42 +0530

    fix connection string issues

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ const seedAdmin = require("./seedAdmin");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/eCommerce");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
     await seedAdmin();
   } catch (error) {
